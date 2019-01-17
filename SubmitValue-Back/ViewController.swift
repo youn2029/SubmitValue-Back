9d26b6b4aecf920f8f73b2767a807303ff3f2bb0
윤성호 <youn2029@naver.com>
2019-01-17 18:06:04 +0900

    첫번째 화면 구성 & viewWillAppear(_:)를 이용하여 화면 표시

--- SubmitValue-Back/ViewController.swift
+++ SubmitValue-Back/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 화면에 표시하기 위한 아우렛 변수
+    @IBOutlet var resultEmail: UILabel!     // 이메일
+    @IBOutlet var resultUpdate: UILabel!    // 자동갱신 여부
+    @IBOutlet var resultInterval: UILabel!  // 갱신주기
+    
+    // 값을 전달받을 프로퍼티
+    var paramEamil : String?
+    var paramUpdate : Bool?
+    var paramIntervar : Double?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // 다른 화면으로 기존 화면을 가렸다가 다시 표시하면 이 메소드가 호출
+    override func viewWillAppear(_ animated: Bool) {
+        
+        if let email = paramEamil {     // nil check
+            resultEmail.text = email
+        }
+        
+        if let update = paramUpdate {
+            resultUpdate.text = update == true ? "자동갱신" : "자동갱신안함"
+        }
+        
+        if let interval = paramIntervar {
+            resultInterval.text = "\(Int(intervar))분 마다"
+        }
     }
 
 
